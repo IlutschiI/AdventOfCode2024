7aee009285429a6ff55c3f61504928a12d40d28f
Lukas Wickenhauser <wicki96@gmail.com>
2024-12-09 17:40:16 +0100

    part2 day08

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -5,6 +5,7 @@ import org.example.readAsString
 fun main() {
 //    val input = "day08/test_input".readAsString()
 //    val input = "day08/test_input2".readAsString()
+//    val input = "day08/test_input3".readAsString()
     val input = "day08/input".readAsString()
 
     val inputWidth = 0..input.lines().first().length-1
@@ -28,13 +29,46 @@ fun main() {
             val antinodes = differences.map { pair.second.first - it.first to pair.second.second - it.second }.filter {
                 it.first in inputHeight && it.second in inputWidth
             }
-//            println(pair)
-//            println(differences)
-//            println(antinodes)
-//            println()
             antinodes
         }
     }.distinct()
 
     println(result.count())
+
+    // Part 2
+    val result2 = groupedAntennas.flatMap { (_, antennas) ->
+        antennas.flatMapIndexed { _, pair ->
+            val differences = antennas.map {
+                it.second.first - pair.second.first to it.second.second - pair.second.second
+            }.filter { it != 0 to 0 }
+
+            val antinodes = differences.flatMap {
+                var newAntinodes= emptyList<Pair<Int,Int>>()
+                var newAntinode = pair.second.first - it.first to pair.second.second - it.second
+
+                var count = 1
+                while (newAntinode.first in inputHeight && newAntinode.second in inputWidth) {
+                    newAntinodes+= newAntinode
+                    count++
+                    newAntinode = pair.second.first - (it.first*count) to pair.second.second - (it.second*count)
+                }
+
+                newAntinode = pair.second.first + it.first to pair.second.second + it.second
+
+                count = 1
+                while (newAntinode.first in inputHeight && newAntinode.second in inputWidth) {
+                    newAntinodes+= newAntinode
+                    count++
+                    newAntinode = pair.second.first + (it.first*count) to pair.second.second + (it.second*count)
+                }
+
+                newAntinodes
+            }.filter {
+                it.first in inputHeight && it.second in inputWidth
+            }
+            antinodes
+        }
+    }.distinct()
+
+    println(result2.count())
 }
