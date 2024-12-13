17f9aceac666d689452eabc90dc216f3fee74d0a
Lukas Wickenhauser <wicki96@gmail.com>
2024-12-13 13:29:04 +0100

    part2 day13

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -3,15 +3,15 @@ package org.example.day13
 import org.example.readAsString
 
 data class Game(
-    val priceX: Int,
-    val priceY: Int,
+    val priceX: Long,
+    val priceY: Long,
     val buttonA: Button,
     val buttonB: Button
 )
 
 data class Button(
-    val x: Int,
-    val y: Int
+    val x: Long,
+    val y: Long
 )
 
 fun main() {
@@ -21,76 +21,76 @@ fun main() {
     val games = input.split(System.lineSeparator() + System.lineSeparator()).map { line ->
         val split = line.split(System.lineSeparator())
         val buttonA =
-            "\\d+".toRegex().findAll(split[0]).let { Button(it.first().value.toInt(), it.last().value.toInt()) }
+            "\\d+".toRegex().findAll(split[0]).let { Button(it.first().value.toLong(), it.last().value.toLong()) }
         val buttonB =
-            "\\d+".toRegex().findAll(split[1]).let { Button(it.first().value.toInt(), it.last().value.toInt()) }
+            "\\d+".toRegex().findAll(split[1]).let { Button(it.first().value.toLong(), it.last().value.toLong()) }
         val game = "\\d+".toRegex().findAll(split[2])
-            .let { Game(it.first().value.toInt(), it.last().value.toInt(), buttonA, buttonB) }
+            .let { Game(it.first().value.toLong(), it.last().value.toLong(), buttonA, buttonB) }
         game
     }
 
     games.mapNotNull { game ->
-        calculateGame(game).also { println(it) }
+        calculateGame(game)
+    }.sum().let { println(it) }
+
+    games.mapNotNull { game ->
+        calculateGamePart2(
+            game.copy(
+                priceX = game.priceX + 10000000000000,
+                priceY = game.priceY + 10000000000000
+            )
+        )
     }.sum().let { println(it) }
 
 }
 
-fun calculateGame(game: Game): Int? {
-    var buttonA: Int? = null
-    var buttonB: Int? = null
-    var buttonA2: Int? = null
-    var buttonB2: Int? = null
+fun calculateGame(game: Game): Long? {
+    val buttonBPresses =
+        (game.buttonA.x * game.priceY - game.buttonA.y * game.priceX) / (game.buttonA.x * game.buttonB.y - game.buttonB.x * game.buttonA.y).toDouble()
+    val buttonAPresses = (game.priceX - game.buttonB.x*buttonBPresses) / game.buttonA.x.toDouble()
 
-    for (i in Math.max(game.priceX / game.buttonA.x, 100) downTo 1) {
-        val currentY = i * game.buttonA.y
-        val currentX = i * game.buttonA.x
-        val yDiffToPrice = game.priceY - currentY
-        val xDiffToPrice = game.priceX - currentX
+    val buttonBPresses2 =
+        (game.buttonA.y * game.priceX - game.buttonA.x * game.priceY) / (game.buttonB.x * game.buttonA.y - game.buttonA.x * game.buttonB.y).toDouble()
+    val buttonAPresses2 = (game.priceX - game.buttonB.x*buttonBPresses2) / game.buttonA.x.toDouble()
 
-
-        if (xDiffToPrice >= 0 && xDiffToPrice % game.buttonB.x == 0 && yDiffToPrice >= 0 && yDiffToPrice % game.buttonB.y == 0) {
-            val buttonBPresses = xDiffToPrice / game.buttonB.x
-            if (currentY+buttonBPresses*game.buttonB.y == game.priceY && currentX+buttonBPresses*game.buttonB.x == game.priceX) {
-                if (i <= 100 && xDiffToPrice / game.buttonB.x <= 100) {
-                    buttonA = i
-                    buttonB = xDiffToPrice / game.buttonB.x
-                    break
-                }
-            }
-        }
+    var result1: Long? = null
+    if (buttonAPresses.rem(1) == 0.0 && buttonBPresses.rem(1) == 0.0 && buttonAPresses <= 100 && buttonBPresses <= 100) {
+        result1 = buttonAPresses.toLong()*3 + buttonBPresses.toLong()
     }
 
-    for (i in Math.max(game.priceX / game.buttonB.x, 100) downTo 1) {
-        val currentY = i * game.buttonB.y
-        val currentX = i * game.buttonB.x
-        val yDiffToPrice = game.priceY - currentY
-        val xDiffToPrice = game.priceX - currentX
-
-        if (xDiffToPrice >= 0 && xDiffToPrice % game.buttonA.x == 0 && yDiffToPrice >= 0 && yDiffToPrice % game.buttonA.y == 0) {
-            val buttonAPresses = xDiffToPrice / game.buttonA.x
-            if (currentY+buttonAPresses*game.buttonA.y == game.priceY && currentX+buttonAPresses*game.buttonA.x == game.priceX) {
-                if (i <= 100 && xDiffToPrice / game.buttonA.x <= 100) {
-                    buttonB2 = i
-                    buttonA2 = xDiffToPrice / game.buttonA.x
-                    break
-                }
-            }
-
-        }
-    }
-
-    var result1:Int? = null
-    if (buttonA != null && buttonB != null) {
-        result1 = buttonA * 3 + buttonB
-    }
-
-    var result2:Int? = null
-    if (buttonA2 != null && buttonB2 != null) {
-        result2 = buttonA2 * 3 + buttonB2
+    var result2: Long? = null
+    if (buttonAPresses2.rem(1) == 0.0 && buttonBPresses2.rem(1) == 0.0 && buttonAPresses2 <= 100 && buttonBPresses2 <= 100) {
+        result2 = buttonAPresses2.toLong()*3 + buttonBPresses2.toLong()
     }
 
     return if (result1 != null || result2 != null) {
-        Math.min(result1?:Int.MAX_VALUE, result2?:Int.MAX_VALUE)
+        Math.min(result1 ?: Long.MAX_VALUE, result2 ?: Long.MAX_VALUE)
+    } else {
+        null
+    }
+}
+
+fun calculateGamePart2(game: Game): Long? {
+    val buttonBPresses =
+        (game.buttonA.x * game.priceY - game.buttonA.y * game.priceX) / (game.buttonA.x * game.buttonB.y - game.buttonB.x * game.buttonA.y).toDouble()
+    val buttonAPresses = (game.priceX - game.buttonB.x*buttonBPresses) / game.buttonA.x.toDouble()
+
+    val buttonBPresses2 =
+        (game.buttonA.y * game.priceX - game.buttonA.x * game.priceY) / (game.buttonB.x * game.buttonA.y - game.buttonA.x * game.buttonB.y).toDouble()
+    val buttonAPresses2 = (game.priceX - game.buttonB.x*buttonBPresses2) / game.buttonA.x.toDouble()
+
+    var result1: Long? = null
+    if (buttonAPresses.rem(1) == 0.0 && buttonBPresses.rem(1) == 0.0) {
+        result1 = buttonAPresses.toLong()*3 + buttonBPresses.toLong()
+    }
+
+    var result2: Long? = null
+    if (buttonAPresses2.rem(1) == 0.0 && buttonBPresses2.rem(1) == 0.0) {
+        result2 = buttonAPresses2.toLong()*3 + buttonBPresses2.toLong()
+    }
+
+    return if (result1 != null || result2 != null) {
+       Math.min(result1 ?: Long.MAX_VALUE, result2 ?: Long.MAX_VALUE)
     } else {
         null
     }
